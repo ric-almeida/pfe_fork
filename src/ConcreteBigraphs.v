39869c35fb29c354421ad581ec71adb4573d651c
c.marcon <cecile.marcon@isae-supaero.fr>
2024-02-14 12:44:19 +0100

    equivalence nat

--- src/ConcreteBigraphs.v
+++ src/ConcreteBigraphs.v
@@ -77,30 +77,32 @@ End GettersBigraphs.
 Section EquivalenceBigraphs.
 
 (** ** On the heterogeneous type *)
-Record bigraph_equality {s1 r1 s2 r2 : FinDecType} {i1 o1 i2 o2 : NoDupList} 
+Record bigraph_equality {s1 r1 s2 r2 : nat} {i1 o1 i2 o2 : NoDupList} 
   (b1 : bigraph s1 i1 r1 o1) (b2 : bigraph s2 i2 r2 o2) : Prop :=
   BigEq
   {
-    bij_s : bijection (type s1) (type s2) ;
+    bij_s : s1 = s2 ;
     bij_i : forall name, In name i1 <-> In name i2 ; (*Permutation i1 i2*)
-    bij_r : bijection (type r1) (type r2) ;
+    bij_r : r1 = r2 ;
     bij_o : forall name, In name o1 <-> In name o2 ;
     bij_n : bijection (type (get_node b1)) (type (get_node b2)) ;
     bij_e : bijection (type (get_edge b1)) (type (get_edge b2)) ;
     bij_p : forall (n1 : type (get_node b1)), bijection (fin (Arity (get_control b1 n1))) (fin (Arity (get_control b2 (bij_n n1)))) ;
     (* TODO Could simply be Arity (get_control b1 n1) = Arity (get_control b2 (bij_n n1))*)
     big_control_eq : (bij_n -->> (@bij_id Kappa)) (get_control b1) = get_control b2 ;
-    big_parent_eq  : ((bij_n <+> bij_s) -->> (bij_n <+> bij_r)) (get_parent b1) = get_parent b2 ;
+    big_parent_eq  : ((bij_n <+> (bij_rew (P := fin) bij_s)) -->> (bij_n <+> ((bij_rew (P := fin) bij_r)))) (get_parent b1) = get_parent b2 ;
     big_link_eq    : ((<{bij_id | bij_i}> <+> <{ bij_n & bij_p }>) -->> (<{bij_id| bij_o}> <+> bij_e)) (get_link b1) = get_link b2
   }.
 
 Lemma bigraph_equality_refl {s r : nat} {i o : NoDupList} (b : bigraph s i r o) :
   bigraph_equality b b.
   Proof.
-  eapply (BigEq _ _ _ _ _ _ _ _ _ _ bij_id _ bij_id _ bij_id bij_id (fun _ => bij_id)).
+  eapply (BigEq _ _ _ _ _ _ _ _ _ _ eq_refl _ eq_refl _ bij_id bij_id (fun _ => bij_id)).
   + rewrite bij_fun_compose_id.
     reflexivity.
-  + rewrite bij_sum_compose_id.
+  + rewrite bij_rew_id.
+    rewrite bij_rew_id.
+    rewrite bij_sum_compose_id.
     rewrite bij_sum_compose_id.
     rewrite bij_fun_compose_id.
     reflexivity.
@@ -116,7 +118,7 @@ Lemma bigraph_equality_refl {s r : nat} {i o : NoDupList} (b : bigraph s i r o) 
   - intros. tauto.
   Qed.
 
-Lemma bigraph_equality_sym {s1 r1 s2 r2 : FinDecType} {i1 o1 i2 o2 : NoDupList}  
+Lemma bigraph_equality_sym {s1 r1 s2 r2 : nat} {i1 o1 i2 o2 : NoDupList}  
   (b1 : bigraph s1 i1 r1 o1) (b2 : bigraph s2 i2 r2 o2) :
   bigraph_equality b1 b2
       -> bigraph_equality b2 b1.
@@ -124,9 +126,9 @@ Lemma bigraph_equality_sym {s1 r1 s2 r2 : FinDecType} {i1 o1 i2 o2 : NoDupList}
   intro Heqb1b2.
   destruct Heqb1b2 as (bij_s, bij_i, bij_r, bij_o, bij_n, bij_e, bij_p, big_control_eq, big_parent_eq, big_link_eq).
   apply (BigEq _ _ _ _ _ _ _ _ b2 b1
-          (bijection_inv bij_s)
+          (eq_sym bij_s)
           (adjunction_equiv bij_id bij_i)
-          (bijection_inv bij_r)
+          (eq_sym bij_r)
           (adjunction_equiv (bij_id) bij_o)
           (bijection_inv bij_n)
           (bijection_inv bij_e)
@@ -142,22 +144,13 @@ Lemma bigraph_equality_sym {s1 r1 s2 r2 : FinDecType} {i1 o1 i2 o2 : NoDupList}
     rewrite id_right_neutral.
     reflexivity.
   + rewrite <- big_parent_eq.
+    rewrite <- bij_inv_rew.
     rewrite <- bij_inv_sum.
+    rewrite <- bij_inv_rew.
     rewrite <- bij_inv_sum.
     rewrite <- bij_inv_fun.
-    simpl.
-    rewrite comp_assoc.
-    rewrite comp_assoc.
-    rewrite parallel_compose.
-    rewrite bij_n.(bof_id _ _).
-    rewrite bij_s.(bof_id _ _).
-    rewrite parallel_id.
-    rewrite id_right_neutral.
-    rewrite <- comp_assoc.
-    rewrite parallel_compose.
-    rewrite bij_n.(bof_id _ _).
-    rewrite bij_r.(bof_id _ _).
-    rewrite parallel_id.
+    rewrite <- bij_compose_forward_simpl.
+    rewrite bij_inv_left_simpl.
     reflexivity.
   + 
     rewrite <- bij_inv_sigT.
@@ -179,7 +172,7 @@ Lemma bigraph_equality_sym {s1 r1 s2 r2 : FinDecType} {i1 o1 i2 o2 : NoDupList}
   Qed.
 
 Lemma bigraph_equality_trans 
-  {s1 r1 s2 r2 s3 r3 : FinDecType} {i1 o1 i2 o2 i3 o3: NoDupList} 
+  {s1 r1 s2 r2 s3 r3 : nat} {i1 o1 i2 o2 i3 o3: NoDupList} 
   (b1 : bigraph s1 i1 r1 o1) (b2 : bigraph s2 i2 r2 o2) (b3 : bigraph s3 i3 r3 o3):
     bigraph_equality b1 b2
       -> bigraph_equality b2 b3  
@@ -189,14 +182,14 @@ Lemma bigraph_equality_trans
   destruct Heqb1b2 as (bij_s12, bij_i12, bij_r12, bij_o12, bij_n12, bij_e12, bij_p12, big_control_eq12, big_parent_eq12, big_link_eq12).
   destruct Heqb2b3 as (bij_s23, bij_i23, bij_r23, bij_o23, bij_n23, bij_e23, bij_p23, big_control_eq23, big_parent_eq23, big_link_eq23).
   apply (BigEq _ _ _ _ _ _ _ _ b1 b3
-          (bij_s23 <O> bij_s12)
+          (eq_trans bij_s12 bij_s23)
           (fun name : Name => @iff_trans 
             (@In Name name (ndlist i1)) 
             (@In Name name (ndlist i2))
             (@In Name (@forward Name Name (@bij_id Name) name) (ndlist i3))
             (bij_i12 name) (bij_i23 (@forward Name Name (@bij_id Name) name))
           )
-          (bij_r23 <O> bij_r12)
+          (eq_trans bij_r12 bij_r23)
           (fun a => iff_trans (bij_o12 a) (bij_o23 (bij_id a)))
           (bij_n23 <O> bij_n12)
           (bij_e23 <O> bij_e12)
@@ -206,10 +199,11 @@ Lemma bigraph_equality_trans
     reflexivity.
   + rewrite <- big_parent_eq23.
     rewrite <- big_parent_eq12.
+    rewrite <- bij_rew_compose.
     rewrite <- bij_sum_compose_compose.
+    rewrite <- bij_rew_compose.
     rewrite <- bij_sum_compose_compose.
     rewrite <- bij_fun_compose_compose.
-    simpl.
     reflexivity.
   + rewrite <- (@bij_subset_compose_compose_id Name (fun name => In name i1) (fun name => In name i2) (fun name => In name i3) bij_i12 bij_i23). 
     rewrite <- (@bij_subset_compose_compose_id Name (fun name => In name o1) (fun name => In name o2) (fun name => In name o3) bij_o12 bij_o23). 
@@ -223,7 +217,7 @@ Lemma bigraph_equality_trans
     reflexivity.
   Qed.
 
-Lemma bigraph_equality_dec {s1 r1 s2 r2 : FinDecType} {i1 o1 i2 o2 : NoDupList}  
+Lemma bigraph_equality_dec {s1 r1 s2 r2 : nat} {i1 o1 i2 o2 : NoDupList}  
   (b1 : bigraph s1 i1 r1 o1) (b2 : bigraph s2 i2 r2 o2) :
   {bigraph_equality b1 b2} + {~ bigraph_equality b1 b2}.
   Proof. 
@@ -238,9 +232,9 @@ Lemma bigraph_equality_dec {s1 r1 s2 r2 : FinDecType} {i1 o1 i2 o2 : NoDupList}
 Record bigraph_packed : Type :=
   mkPacked
   {
-  s: FinDecType;
+  s: nat;
   i: NoDupList;
-  r: FinDecType;
+  r: nat;
   o: NoDupList;
   big : bigraph s i r o
   }.
@@ -266,7 +260,7 @@ Lemma bigraph_packed_equality_trans (bp1 bp2 bp3 : bigraph_packed) : bigraph_pac
   apply bigraph_equality_trans.
   Qed. 
 
-Record support_equivalent {s1 r1 s2 r2 : FinDecType} {i1 o1 i2 o2 : NoDupList} 
+Record support_equivalent {s1 r1 s2 r2 : nat} {i1 o1 i2 o2 : NoDupList} 
   (b1 : bigraph s1 i1 r1 o1) (b2 : bigraph s2 i2 r2 o2) : Prop :=
   SupEq
   {
@@ -303,7 +297,7 @@ Notation "l1 # l2" := (Disjoint l1 l2) (at level 50, left associativity).
   nodes & edges are automatically disjoint bc + is a disjoint sum
   On this we can prove associativity, neutral elt, and distribution to composition (M1 M2 M3 in Milner's book p21) 
   But bascially i'ts just juxtaposition with added hypothesis*)
-Definition bigraph_tensor_product {s1 r1 s2 r2 : FinDecType} {i1 o1 i2 o2 : NoDupList} 
+Definition bigraph_tensor_product {s1 r1 s2 r2 : nat} {i1 o1 i2 o2 : NoDupList} 
   {dis_i : i1 # i2}
   {dis_o : o1 # o2}
   (b1 : bigraph s1 i1 r1 o1) (b2 : bigraph s2 i2 r2 o2) 
@@ -343,7 +337,7 @@ Remark void_disjoint_all_list_right : forall l:NoDupList, l # EmptyNDL.
 (*juxtaposition, also called parallel product
   in the book, parallel product is defined from tensor product p33 with the sentence "is defined just as tensor product, except that its link map allows name-sharing"
   but I think we should probably do it the other way around *)
-Definition bigraph_juxtaposition {s1 r1 s2 r2 : FinDecType} {i1 o1 i2 o2 : NoDupList} 
+Definition bigraph_juxtaposition {s1 r1 s2 r2 : nat} {i1 o1 i2 o2 : NoDupList} 
   (b1 : bigraph s1 i1 r1 o1) (b2 : bigraph s2 i2 r2 o2)
     : bigraph (findec_sum s1 s2) (app_merge_NoDupList i1 i2) (findec_sum r1 r2) (app_merge_NoDupList o1 o2).
   Proof.
