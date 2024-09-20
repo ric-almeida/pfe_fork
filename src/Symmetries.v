e79ff30a921c30fbf510711f9495af04a22af28a
c.marcon <cecile.marcon@isae-supaero.fr>
2024-09-20 16:14:26 +0200

    place axioms done

--- src/Symmetries.v
+++ src/Symmetries.v
@@ -619,10 +619,6 @@ End SymmetryAxiom.
     
 Section PlaceAxioms.
 
-(* Place axioms: 
-  join ◦ γ1,1 = join 
-  join ◦ (1 ⊗ id1)=id1 
-  join ◦ (join ⊗ id1)=join ◦ (id1 ⊗ join) *)
 
 Lemma place_axiom_join_sym1_1 : 
   bigraph_equality
@@ -655,6 +651,87 @@ Lemma place_axiom_join_sym1_1 :
     - elim i0.    
     - simpl in v. destruct v.
   Qed.
+
+Lemma place_axiom_join_1_id_1 : 
+  bigraph_equality
+    (join_big <<o>> (big_1 ⊗ bigraph_id 1 EmptyNDL))
+    (bigraph_id 1 EmptyNDL).
+  Proof.
+  refine (
+    BigEq _ _ _ _ _ _ _ _
+      (join_big <<o>> (big_1 ⊗ bigraph_id 1 EmptyNDL))
+      (bigraph_id 1 EmptyNDL)
+      (esym (addn0 (1))) (*s*)
+      (permutation_left_neutral_neutral) (*i*)
+      (erefl) (*r*)
+      (permutation_left_neutral_neutral) (*o*)
+      (bijection_inv bij_void_sum_sum) (*n*)
+      (bijection_inv bij_void_sum_sum) (*e*)
+      (fun n => match n with 
+      | inl n => bij_rew (void_univ_embedding n) (*p*)
+      | inr n => match n with 
+        | inl n => bij_rew (void_univ_embedding n)
+        | inr n => bij_rew (void_univ_embedding n)
+        end
+      end) (*p*)
+      _ _ _
+    ).
+  + apply functional_extensionality.
+    destruct x.
+  + simpl. apply functional_extensionality.
+    destruct x as [v | [s1]]; try destruct v; simpl.
+    unfold funcomp, parallel,rearrange. simpl.
+    unfold sum_shuffle,rearrange,fintype.split.
+    unfold bij_rew_forward.
+    rewrite eq_rect_ordinal. simpl. 
+    destruct (ltnP s1 0).
+    - discriminate i1.
+    - f_equal. apply val_inj. simpl. apply lt1_eq0 in i0. auto.
+  + apply functional_extensionality.
+    destruct x as [[name] | (v, tmp)]; simpl.
+    - elim i0.    
+    - simpl in v. destruct v.
+  Qed.
+
+
+Lemma place_axiom_join_id_commutes : 
+  bigraph_equality
+    (join_big <<o>> (join_big ⊗ bigraph_id 1 EmptyNDL))
+    (join_big <<o>> (bigraph_id 1 EmptyNDL ⊗ join_big)).
+  Proof.
+  refine (
+    BigEq _ _ _ _ _ _ _ _
+      (join_big <<o>> (join_big ⊗ bigraph_id 1 EmptyNDL))
+      (join_big <<o>> (bigraph_id 1 EmptyNDL ⊗ join_big))
+      (addnC 1 2) (*s*)
+      (permutation_left_neutral_neutral) (*i*)
+      (erefl) (*r*)
+      (permutation_left_neutral_neutral) (*o*)
+      (bij_id) (*n*)
+      (bij_id) (*e*)
+      (fun n => match n with 
+      | inl n => bij_rew (void_univ_embedding n) (*p*)
+      | inr n => match n with 
+        | inl n => bij_rew (void_univ_embedding n)
+        | inr n => bij_rew (void_univ_embedding n)
+        end
+      end) (*p*)
+      _ _ _
+    ).
+  + apply functional_extensionality.
+    destruct x as [v|[v|v]]; destruct v.
+  + simpl. apply functional_extensionality.
+    destruct x as [[v|[v|v]] | [s1]]; try destruct v; simpl.
+    unfold funcomp, parallel,rearrange. simpl.
+    unfold sum_shuffle,rearrange,fintype.split.
+    unfold bij_rew_forward.
+    rewrite eq_rect_ordinal. simpl. 
+    destruct (ltnP s1 1); destruct (ltnP s1 2);reflexivity.
+  + apply functional_extensionality.
+    destruct x as [[name] | ([v|[v|v]], tmp)]; simpl; try simpl in v; try destruct v.
+    - elim i0.   
+  Qed.
+
 
 End PlaceAxioms.
 
